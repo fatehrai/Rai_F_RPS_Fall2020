bba69ce1498ce971cdd3e5ed47ff372318175eda
fatehrai <rai.fateh30@gmail.com>
2020-11-28 23:27:50 -0500

    updated game

--- gameComponents/comparisons.py
+++ gameComponents/comparisons.py
@@ -2,34 +2,38 @@ from random import randint
 
 from gameComponents import gameVars
 
-computer = gameVars.choices[randint(0,2)]
-
 def comparisonfn(status):
+
+
+	computer = gameVars.choices[randint(0,2)]
+
+
 	
-		if (computer == gameVars.player):
-			print("tie")
-		# always check for negative conditions first (the losing case)
-		elif (computer == "rock"):
-			if (gameVars.player == "scissors"):
-				gameVars.player_lives -= 1
-				print("you lose! gameVars.player lives: ", gameVars.player_lives)
-			else:
-				print("you win!")
-				gameVars.computer_lives -= 1
+	if (computer == gameVars.player):
+		print("tie")
 
-		elif (computer == "paper"):
-			if (gameVars.player == "rock"):
-				gameVars.player_lives -= 1
-				print("you lose! gameVars.player lives: ", gameVars.player_lives)
-			else:
-				print("you win!")
-				gameVars.computer_lives -= 1
+	# always check for negative conditions first (the losing case)
+	elif (computer == "rock"):
+		if (gameVars.player == "scissors"):
+			gameVars.player_lives -= 1
+			print("you lose! gameVars.player lives: ", gameVars.player_lives)
+		else:
+			print("you win!")
+			gameVars.computer_lives -= 1
 
-		elif (computer == "scissors"):
-			if (gameVars.player == "paper"):
-				gameVars.player_lives -= 1
-				print("you lose! gameVars.player lives: ", gameVars.player_lives)
-			else:
-				print("you win!")
-				gameVars.computer_lives -= 1
-			
+	elif (computer == "paper"):
+		if (gameVars.player == "rock"):
+			gameVars.player_lives -= 1
+			print("you lose! gameVars.player lives: ", gameVars.player_lives)
+		else:
+			print("you win!")
+			gameVars.computer_lives -= 1
+
+	elif (computer == "scissors"):
+		if (gameVars.player == "paper"):
+			gameVars.player_lives -= 1
+			print("you lose! gameVars.player lives: ", gameVars.player_lives)
+		else:
+			print("you win!")
+			gameVars.computer_lives -= 1
+				
